fb8c06842adfa4fe8bedba79858bcf635b8677b5
fioritjx <fioritjx@rose-hulman.edu>
2022-09-24 16:43:06 -0400

    More DFLs

--- src/DynamicFitnessLandscapeAnalyzer.java
+++ src/DynamicFitnessLandscapeAnalyzer.java
@@ -4,13 +4,17 @@ import java.io.PrintWriter;
 
 public class DynamicFitnessLandscapeAnalyzer {
 	public static void main(String[] args) throws FileNotFoundException {
-//		DFLfitOverTime(new XorFitnessLandscape(15,10,.1,0),"fitnessOverCyclesXor.csv");
-//		DFLfitOverTime(new LERPFitnessLandscape(15,10,100,0),"fitnessOverCyclesLERP.csv");
-//		DFLfitOverTime(new PLTFitnessLandscape(15,10,.1,0),"fitnessOverCyclesPLT.csv");
+		DFLfitOverTime(new XorFitnessLandscape(15,10,.1,0),"fitnessOverCyclesXor.csv");
+		DFLfitOverTime(new LERPFitnessLandscape(15,10,100,0),"fitnessOverCyclesLERP.csv");
+		DFLfitOverTime(new PLTFitnessLandscape(15,10,.1,0),"fitnessOverCyclesPLT.csv");
 		DFLfitOverTime(new CopyFitnessLandscape(15,10,.2,0),"fitnessOverCyclesCopy.csv");
+		DFLfitOverTime(new TempPermFitnessLandscape(15,10,5,3,0),"fitnessOverCyclesTempPerm.csv");
+		DFLfitOverTime(new SingleTempFitnessLandscape(15,10,1,0),"fitnessOverCyclesSingleTemp.csv");
+		DFLfitOverTime(new SumTempFitnessLandscape(15,10,5,3,.1,0),"fitnessOverCyclesSumTemp.csv");		
 	}
 	
 	public static void DFLfitOverTime(DynamicFitnessLandscape DFL, String filename) throws FileNotFoundException {
+		long start = System.nanoTime();
 		PrintWriter writer = new PrintWriter (new File(filename));
 		for(int i = 0; i<100; i++) {
 			double dfl = DFL.fitness(0);
@@ -25,5 +29,6 @@ public class DynamicFitnessLandscapeAnalyzer {
 		}
 		writer.flush();
 		writer.close();
+		System.out.println(filename+": "+(System.nanoTime()-start));
 	}
 }
